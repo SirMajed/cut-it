6cf780344e472d75dea994378d9e6c636268d851
SirMajed <sidrozzg@gmail.com>
2022-10-19 20:56:18 +0300

    Add copy to clipboard button

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,15 @@ import axios from "axios";
 import { useState } from "react";
 import svgIcon from "../public/icons/cut.svg";
 import { TbFidgetSpinner } from "react-icons/tb";
+import { MdContentCopy, MdCheck } from "react-icons/md";
 import Head from "next/head";
 import Image from "next/image";
+import copy from "copy-to-clipboard";
 export default function Home() {
   const [url, setUrl] = useState("");
   const [loading, setLoading] = useState(false);
   const [shortUrl, setShortUrl] = useState("");
+  const [copied, setCopied] = useState(false);
   const cutUrl = async (e: any) => {
     e.preventDefault();
     try {
@@ -24,61 +27,105 @@ export default function Home() {
       console.log(error);
     }
   };
+
+  const copyUrl = () => {
+    setCopied(true);
+    copy(shortUrl);
+    setTimeout(() => {
+      setCopied(false);
+    }, 2300);
+  };
+
+  const shortenAnotherUrl = () => {
+    setShortUrl("");
+  };
   return (
     <>
-    <Head>
-    <title>CUT-IT URL Shorener</title>
+      <Head>
+        <title>CUT-IT URL Shorener</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
-    <div className="flex h-screen w-full ">
-      <div className="flex flex-col items-center justify-center bg-white w-1/2">
-        <div className="w-3/4">
-          <h1 className="text-7xl text-sky-800 font-primary">CUT IT</h1>
-          <p className="p-2 font-primary">
-            CUT IT is free shorten url service provided by{" "}
-            <a
-              href="https://urlo.in"
-              target={"noopener noreferrer"}
-              className="italic text-sky-800 underline cursor-pointer"
-            >
-              urlo.in
-            </a>{" "}
-            and this website is just a simple minify design by Majed
-            Alhasin 🫡
-          </p>
-          {shortUrl ? (
-            <h1>{shortUrl}</h1>
-          ) : (
-            <div>
-              <input
-                onChange={(e: any) => setUrl(e.target.value)}
-                placeholder="Enter URL"
-                className="p-3 rounded-md w-full focus:outline-none appearance-none focus:border-sky-800 transition focus:ring-0"
-                type="text"
-              />
-              <button
-                onClick={cutUrl}
-                className="bg-sky-700 hover:bg-sky-800 transition text-white px-3 py-1.5 mt-4 rounded-md"
+      </Head>
+      <div className="flex h-screen w-full ">
+        <div className="flex flex-col items-center justify-center bg-white w-1/2">
+          <div className="w-3/4">
+            <h1 className="text-7xl text-sky-800 font-primary">CUT IT</h1>
+            <p className="px-1 py-2 font-primary">
+              CUT IT is free shorten url service provided by
+              <a
+                href="https://urlo.in"
+                target={"noopener noreferrer"}
+                className="italic text-sky-800 underline cursor-pointer"
               >
-                {loading ? (
-                  <TbFidgetSpinner
-                    className="text-white animate-spin"
+                urlo.in
+              </a>{" "}
+              and this website is just a simple minify design by Majed Alhasin 🫡
+            </p>
+            {shortUrl ? (
+              <div>
+                <div className="p-2 bg-sky-100 rounded-md shadow-sm flex justify-between items-center">
+                  <h1 className="mx-2">
+                    {copied ? (
+                      <span className="text-sky-600 flex items-center gap-1">
+                        Link copied to clipboard{" "}
+                        <MdCheck className="text-sky-800" size={20} />{" "}
+                      </span>
+                    ) : (
+                      shortUrl
+                    )}
+                  </h1>
+                  <MdContentCopy
+                    onClick={copyUrl}
                     size={20}
+                    className="cursor-pointer text-sky-800"
                   />
-                ) : (
-                  "Shorten URL"
-                )}
-              </button>
-            </div>
-          )}
+                </div>
+                <button
+                  onClick={shortenAnotherUrl}
+                  className="bg-sky-700 hover:bg-sky-800 transition text-white px-3 py-1.5 mt-4 rounded-md"
+                >
+                  Shorten another URL
+                </button>
+              </div>
+            ) : (
+              <div>
+                <form onSubmit={cutUrl}>
+                <input
+                disabled={loading}
+                  onChange={(e: any) => setUrl(e.target.value)}
+                  placeholder="Enter URL"
+                  className="p-3 rounded-md w-full focus:outline-none appearance-none focus:border-sky-800 transition focus:ring-0"
+                  type="text"
+                />
+                <button
+                type="submit"
+                  className="bg-sky-700 hover:bg-sky-800 transition text-white px-3 py-1.5 mt-4 rounded-md"
+                >
+                  {loading ? (
+                    <TbFidgetSpinner
+                      className="text-white animate-spin"
+                      size={20}
+                    />
+                  ) : (
+                    "Shorten URL"
+                  )}
+                </button>
+                </form>
+              </div>
+            )}
+          </div>
+        </div>
+
+        <div className="flex flex-col  gap-5 items-center justify-center bg-sky-500 w-1/2">
+          <Image src={svgIcon.src} height={200} width={200} alt="" />
+          <a
+            href="https://www.linkedin.com/in/alhasenmajed/"
+            target={"noopener noreferrer"}
+            className="italic opacity-10"
+          >
+            By Majed Alhasin
+          </a>
         </div>
       </div>
-
-      <div className="flex flex-col  gap-5 items-center justify-center bg-sky-500 w-1/2">
-        <Image src={svgIcon.src} height={200} width={200} alt="" />
-        <a href="https://www.linkedin.com/in/alhasenmajed/" target={"noopener noreferrer"} className="italic opacity-10">By Majed Alhasin</a>
-      </div>
-    </div>
     </>
   );
 }
